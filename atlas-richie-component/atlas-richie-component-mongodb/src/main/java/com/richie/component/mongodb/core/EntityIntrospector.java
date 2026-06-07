40260a98a75e8282c67708ea00d7b19d7fac8644
richie696 <richie696@icloud.com>
2026-06-07 10:24:59 +0800

    feat(mongodb): 扩展 EntityIntrospector 缓存注解元数据

--- atlas-richie-component/atlas-richie-component-mongodb/src/main/java/com/richie/component/mongodb/core/EntityIntrospector.java
+++ atlas-richie-component/atlas-richie-component-mongodb/src/main/java/com/richie/component/mongodb/core/EntityIntrospector.java
@@ -1,5 +1,9 @@
 package com.richie.component.mongodb.core;
 
+import com.richie.component.mongodb.annotation.AuditFields;
+import com.richie.component.mongodb.annotation.ExpireAfter;
+import com.richie.component.mongodb.annotation.SoftDelete;
+import com.richie.component.mongodb.annotation.TenantScoped;
 import com.richie.component.mongodb.builder.LambdaField;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
@@ -28,6 +32,22 @@ public class EntityIntrospector {
         return cache.computeIfAbsent(entityClass, this::buildMeta).indexedFields;
     }
 
+    public String getSoftDeleteField(Class<?> entityClass) {
+        return cache.computeIfAbsent(entityClass, this::buildMeta).softDeleteField;
+    }
+
+    public String getTenantField(Class<?> entityClass) {
+        return cache.computeIfAbsent(entityClass, this::buildMeta).tenantField;
+    }
+
+    public boolean hasAuditFields(Class<?> entityClass) {
+        return cache.computeIfAbsent(entityClass, this::buildMeta).hasAuditFields;
+    }
+
+    public List<Field> getExpireAfterFields(Class<?> entityClass) {
+        return cache.computeIfAbsent(entityClass, this::buildMeta).expireAfterFields;
+    }
+
     public String resolveFieldName(LambdaField<?, ?> lambda) {
         return LambdaMeta.resolveFieldName(lambda);
     }
@@ -40,6 +60,18 @@ public class EntityIntrospector {
 
         String idField = "id";
         List<Field> indexedFields = new ArrayList<>();
+        List<Field> expireAfterFields = new ArrayList<>();
+        String softDeleteField = null;
+        String tenantField = null;
+        boolean hasAuditFields = false;
+
+        if (clazz.isAnnotationPresent(SoftDelete.class)) {
+            softDeleteField = clazz.getAnnotation(SoftDelete.class).value();
+        }
+        if (clazz.isAnnotationPresent(TenantScoped.class)) {
+            tenantField = clazz.getAnnotation(TenantScoped.class).value();
+        }
+        hasAuditFields = clazz.isAnnotationPresent(AuditFields.class);
 
         for (Field field : clazz.getDeclaredFields()) {
             if (field.isAnnotationPresent(Id.class)) {
@@ -48,10 +80,15 @@ public class EntityIntrospector {
             if (field.isAnnotationPresent(Indexed.class)) {
                 indexedFields.add(field);
             }
+            if (field.isAnnotationPresent(ExpireAfter.class)) {
+                expireAfterFields.add(field);
+            }
         }
 
-        return new EntityMeta(collectionName, idField, indexedFields);
+        return new EntityMeta(collectionName, idField, indexedFields, softDeleteField, tenantField, hasAuditFields, expireAfterFields);
     }
 
-    record EntityMeta(String collectionName, String idField, List<Field> indexedFields) {}
+    record EntityMeta(String collectionName, String idField, List<Field> indexedFields,
+                      String softDeleteField, String tenantField, boolean hasAuditFields,
+                      List<Field> expireAfterFields) {}
 }
